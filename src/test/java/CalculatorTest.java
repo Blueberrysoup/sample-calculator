7ca98a9a242e965d21f133d18c4550e1e818a6ab
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-07 17:34:29 +0200

    Finished test cases for all methods except division by zero

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertEquals;
 
-import java.text.DecimalFormat;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -59,11 +58,9 @@ public class CalculatorTest {
 
 	@Test
 	public void testAdd_zero() {
-		for (int i = 0; i < 200; i++) {
-			double expected = 0.0 + 0.0;
-			LOG.info("Testing the add method with 0 + 0");
-			assertEquals(expected, calc.add(0, 0), 0.01);
-		}	
+		double expected = 0.0 + 0.0;
+		LOG.info("Testing the add method with 0 + 0");
+		assertEquals(expected, calc.add(0, 0), 0.01);
 	}
 
 	@Test
@@ -77,7 +74,10 @@ public class CalculatorTest {
 			rand2 = Math.round(random.nextDouble() * -1000)/100.00;
 			expected = rand1 + rand2;
 			LOG.info("Testing the add method with: " + rand1 + " and " + rand2);
-			assertEquals(expected, calc.add(rand1, rand2), 0.01);
+			if (i == 136)
+				assertEquals(expected, calc.add(rand1, rand2), 0.01);
+			else	
+				assertEquals(expected, calc.add(rand1, rand2), 0.01);
 		}	
 	}
 
@@ -108,6 +108,297 @@ public class CalculatorTest {
 			expected = rand1 + rand2;
 			LOG.info("Testing the add method with: " + rand1 + " and " + rand2);
 			assertEquals(expected, calc.add(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_smallPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_mediumPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 10000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_largePositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_zero() {
+		double expected = 0.0 - 0.0;
+		LOG.info("Testing the subtract method with 0 - 0");
+		assertEquals(expected, calc.subtract(0, 0), 0.01);
+	}
+
+	@Test
+	public void testSubtract_smallNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_mediumNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -10000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testSubtract_largeNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000000)/100.00;
+			expected = rand1 - rand2;
+			LOG.info("Testing the subtract method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.subtract(rand1, rand2), 0.01);
+		}	
+	}
+	
+	@Test
+	public void testMultiply_smallPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testMultiply_mediumPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 10000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testMultiply_largePositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testMultiply_zero() {
+		double expected = 0.0 * 0.0;
+		LOG.info("Testing the multiply method with 0 * 0");
+		assertEquals(expected, calc.multiply(0, 0), 0.01);
+	}
+
+	@Test
+	public void testMultiply_smallNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testMultiply_mediumNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -10000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testMultiply_largeNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000000)/100.00;
+			expected = rand1 * rand2;
+			LOG.info("Testing the multiply method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.multiply(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testDivide_smallPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testDivide_mediumPositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 10000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testDivide_largePositive() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * 1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * 1000000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
+		}	
+	}
+
+/*	@Test
+	public void testDivide_zero() {
+		double expected = 0;
+		LOG.info("Testing the subtract method with 0 / 0");
+		assertEquals(expected, calc.divide(0, 0), 0.01);
+	}*/
+
+	@Test
+	public void testDivide_smallNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testDivide_mediumNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -10000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -10000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
+		}	
+	}
+
+	@Test
+	public void testDivide_largeNegative() {
+		double rand1 = 0;
+		double rand2 = 0;
+		double expected = 0;
+		
+		for (int i = 0; i < 200; i++) {
+			rand1 = Math.round(random.nextDouble() * -1000000)/100.00;
+			rand2 = Math.round(random.nextDouble() * -1000000)/100.00;
+			expected = rand1 / rand2;
+			LOG.info("Testing the divide method with: " + rand1 + " and " + rand2);
+			assertEquals(expected, calc.divide(rand1, rand2), 0.01);
 		}	
 	}
 }
